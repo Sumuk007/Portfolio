3ddd5aef4ca942e5d849463ee9731390bc0429a4
Sumuk <sumukbhat007@gmail.com>
2025-07-13 18:45:30 +0530

    Final Changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Github, ExternalLink, Code, Laptop } from "lucide-react";
-import ai_resume from "../assets/projects/ai-resume_analyzer.png";
+import ai_resume from "../assets/projects/ai_resume_analyzer.png";
 import reelninja from "../assets/projects/reelninja.png";
 import packageinfo from "../assets/projects/package_info.png";
 
